d345d0431eb4e03d705964fc8a803a69f5f8e45f
lden <denis.lednyov@gmail.com>
2022-01-30 09:49:14 +0300

    add test

--- src/tests/milk/test_milk_positive.py
+++ src/tests/milk/test_milk_positive.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 import pytest
 from src.example.create_order import OrderDirector
+
+director = OrderDirector()
+director.construct_order("milk", 3, "SELF_MADE", g_tin= "1241415235")
+order = director.order
+order.create_order()
+print(order.orderId)
+print(order)
